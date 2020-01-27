f6ffde3cdee14c450dc695787ac5b94db4f002ae
Kion <daniarkano@gmail.com>
2020-01-27 22:32:43 +0100

    Implementing Ingredients Add Update

---
+++ src/test/java/guru/springframework/services/IngredientServiceImplTest.java
@@ -0,0 +1,78 @@
+package guru.springframework.services;
+
+import guru.springframework.commands.IngredientCommand;
+import guru.springframework.converters.IngredientToIngredientCommand;
+import guru.springframework.converters.UnitOfMeasureToUnitOfMeasureCommand;
+import guru.springframework.domain.Ingredients;
+import guru.springframework.domain.Recipe;
+import guru.springframework.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+
+public class IngredientServiceImplTest {
+
+    private final IngredientToIngredientCommand ingredientToIngredientCommand;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    IngredientService ingredientService;
+
+    public IngredientServiceImplTest(){
+        this.ingredientToIngredientCommand = new IngredientToIngredientCommand(new UnitOfMeasureToUnitOfMeasureCommand());
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        ingredientService = new IngredientServiceImpl(ingredientToIngredientCommand, recipeRepository);
+    }
+
+    @Test
+    public void findByRecipeIdAndIngredientId() {
+    }
+
+    @Test
+    public void findByRecipeIdAndIdHappyPath() throws Exception{
+        //given
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        Ingredients ingredient1 = new Ingredients();
+        ingredient1.setId(1L);
+
+        Ingredients ingredient2 = new Ingredients();
+        ingredient2.setId(1L);
+
+        Ingredients ingredient3 = new Ingredients();
+        ingredient3.setId(3L);
+
+        recipe.addIngredient(ingredient1);
+        recipe.addIngredient(ingredient2);
+        recipe.addIngredient(ingredient3);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        //then
+        IngredientCommand ingredientCommand = ingredientService.findByRecipeIdAndIngredientId(1L, 3L);
+
+        //when
+        assertEquals(Long.valueOf(3L), ingredientCommand.getId());
+        assertEquals(Long.valueOf(1L), ingredientCommand.getRecipeId());
+        verify(recipeRepository, times(1)).findById(anyLong());
+    }
+}
